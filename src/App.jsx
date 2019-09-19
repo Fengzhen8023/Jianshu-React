ec6ad2e95f8b5f2ed0fb076e6a469d5ff03f2681
Zhenzhen Feng <zfeng020@nam.ad.pwcinternal.com>
2019-09-19 11:42:44 +0800

    add the page header content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
 import Header from 'components/header/Header'
-import Content from 'components/content/Content'
-import Find from 'views/find/Find'
-import Care from 'views/care/Care'
-import Message from 'views/message/Message'
-import Favorite from 'views/favorite/Favorite'
-import Help from 'views/help/Help'
-import Like from 'views/like/Like'
-import PersonIndex from 'views/personIndex/PersonIndex'
-import Purchase from 'views/purchase/Purchase'
-import Set from 'views/set/Set'
-import Wallet from 'views/wallet/Wallet'
+import './asset/css/init.css'
+
 
 class App extends React.Component {
     render(){
         return (
             <div>
-                <BrowserRouter>
-                    <Header></Header>
-                    <Content>
-                        <Route path="/find" component={ Find } />
-                        <Route path="/care" component={ Care } />
-                        <Route path="/message" component={ Message } />
-                        <Route path="/favorite" component={ Favorite } />
-                        <Route path="/help" component={ Help } />
-                        <Route path="/like" component={ Like } />
-                        <Route path="/personIndex" component={ PersonIndex } />
-                        <Route path="/purchase" component={ Purchase } />
-                        <Route path="/set" component={ Set } />
-                        <Route path="/wallet" component={ Wallet } />
-                    </Content> 
-                </BrowserRouter>
+                <Header></Header>
             </div>
         );
     }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,90 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './Header.scss'
+import { Icon } from 'antd'
 
+const logo = require('images/jianshu-logo.png');
+const face = require('images/face-photo.png');
+const beat = require('images/beat.png');
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeNavIndex: 0,
+            searchText: ""
+        }
+    }
+
     render() {
         return (
-            <ul className="route-list">
-                <li><Link to="/find">发现</Link></li>
-                <li><Link to="/care">关注</Link></li>
-                <li><Link to="/message">消息</Link></li>
-                <li><Link to="/favorite">收藏的文章</Link></li>
-                <li><Link to="/help">帮助</Link></li>
-                <li><Link to="/like">喜欢的文章</Link></li>
-                <li><Link to="/personIndex">个人主页</Link></li>
-                <li><Link to="/purchase">购买的文章</Link></li>
-                <li><Link to="/set">设置</Link></li>
-                <li><Link to="/wallet">钱包</Link></li>
-            </ul>
+            <div className="header-nav">
+                <div className="left-part">
+                    <img src={logo} alt="logo" className="logo"/>
+                </div>
+                <div className="middle-part">
+                    <ul className="nav-list">
+                        <li 
+                            onClick={() => this.changeActiveNav(0)}
+                            className={`nav-item ${this.state.activeNavIndex === 0 ? "active-nav" : "no-active-nav"}`}>
+                            <Icon type="compass" />
+                            <span>发现</span>
+                        </li>
+                        <li 
+                            onClick={() => this.changeActiveNav(1)}
+                            className={`nav-item ${this.state.activeNavIndex === 1 ? "active-nav" : "no-active-nav"}`}>
+                            <Icon type="book" />
+                            <span>关注</span>
+                        </li>
+                        <li     
+                            onClick={() => this.changeActiveNav(2)}
+                            className={`nav-item ${this.state.activeNavIndex === 2 ? "active-nav" : "no-active-nav"}`}>
+                            <Icon type="bell" />
+                            <span>消息</span>
+                        </li>
+                        <li className="search-input">
+                            <input type="text" placeholder="搜索" onKeyUp={(e) => this.handleInputSearch(e)} />
+                            <Icon type="search" className="search-icon" onClick={this.searchArticle} />
+                        </li>
+                    </ul>
+                </div>
+                <div className="right-part">
+                    <div className="font-set">
+                        <Icon type="font-colors" />
+                    </div>
+                    <div className="diamond">
+                        <img src={beat} alt="beat"/>
+                    </div>
+                    <div className="photo">
+                        <img src={face} alt="face"/>
+                        <Icon type="caret-down" />
+                    </div>
+                    <div className="write-article">
+                        <Icon type="audio" className="write-article-icon" />
+                        <span>写文章</span>
+                    </div>
+                </div>
+            </div>
         )
+    }
+
+    changeActiveNav = (index) => {
+        this.setState({
+            activeNavIndex: index
+        });
+    }
+
+    handleInputSearch = (e) => {
+        if(e.keyCode === 13) {
+            this.searchArticle();
+            return false;
+        }
+        this.setState({
+            searchText: e.currentTarget.value
+        });
+    }
+
+    searchArticle = () => {
+        console.log("当前搜索框的值： ", this.state.searchText);
     }
 }
 
